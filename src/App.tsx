a7a4ff4b13bcd32c85cdc8fcdbcaf0f7a0da8fda
lsmxy4 <lsmxy4@gmail.com>
2026-07-01 12:02:59 +0900

    feat : ArchitecturePage Add

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import BlankPage from './pages/BlankPage'
+import ArchitecturePage from './pages/ArchitecturePage'
 import SupportPage from './pages/SupportPage'
 import SupportNewPage from './pages/SupportNewPage'
 import SupportDetailPage from './pages/SupportDetailPage'
@@ -15,7 +16,7 @@ const router = createBrowserRouter([
       // 팀원이 채워넣을 빈 라우트 (홈에서 전체 디자인 참고 가능)
       { path: '/about', element: <BlankPage /> },
       { path: '/service', element: <BlankPage /> },
-      { path: '/architecture', element: <BlankPage /> },
+      { path: '/architecture', element: <ArchitecturePage /> },
       { path: '/monitoring', element: <BlankPage /> },
       { path: '/support', element: <SupportPage /> },
       { path: '/support/new', element: <SupportNewPage /> },
